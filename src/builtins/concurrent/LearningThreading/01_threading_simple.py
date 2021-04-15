73212757d156d640973c671a1a71a5850f978847
sugarP1G <zhuchenhui1990@163.com>
2021-04-15 11:33:12 +0800

    Create 01_threading_simple.py

---
+++ src/builtins/concurrent/LearningThreading/01_threading_simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import threading
+
+
+def worker():
+    """thread worker function"""
+    print('Worker')
+
+
+threads = []
+for i in range(5):
+    t = threading.Thread(target=worker)
+    threads.append(t)
+    t.start()
