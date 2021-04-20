cac199135eeb35c9e376cabecb6e76e7ec7abe69
sugarP1G <zhuchenhui1990@163.com>
2021-04-20 15:09:44 +0800

    Create doctest_func.py

---
+++ src/tests/LearningDoctest/doctest_func.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def abs(n):
+    """
+    Function to get absolute value of number.
+
+    Example:
+
+    >>> abs(1)
+    1
+    >>> abs(-1)
+    1
+    >>> abs(0)
+    0
+    """
+    return n if n >= 0 else (-n)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
